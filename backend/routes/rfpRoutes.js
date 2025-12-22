a15e82bb369f57454e23b6b804ce2729c1bfff46
omprakashsingh <munnato822@mail.com>
2025-12-23 01:40:33 +0530

    Fix: Reorder routes to prioritize specific paths over parameters

--- backend/routes/rfpRoutes.js
+++ backend/routes/rfpRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const rfpController = require('../controllers/rfpController');
 
+router.post('/send', rfpController.sendRFPToVendors);
 router.post('/', rfpController.createRFP);
 router.get('/', rfpController.getAllRFPs);
 router.get('/:id', rfpController.getRFPById);
-router.post('/send', rfpController.sendRFPToVendors);
 
 module.exports = router;
